3599e314ba4e1cc21940f8761c69e9679bd25584
Olexander Zakatov <sashazakatov17@gmail.com>
2023-08-09 14:39:20 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AvatarController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::prefix('/users')->name('users.')->group(function(){
 
     //возрошает пользователей
