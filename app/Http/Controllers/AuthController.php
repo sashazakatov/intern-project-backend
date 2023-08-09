271edc0dfa577d981e59c868d872f7d9740b1b08
Olexander Zakatov <sashazakatov17@gmail.com>
2023-08-09 21:47:40 +0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Http\Requests\UserCreateRequest;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(UserCreateRequest $request)
     {
         $credentials = $request->only('email', 'password');
 
