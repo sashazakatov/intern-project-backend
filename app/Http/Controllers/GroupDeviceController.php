3137ec3b046a1477e065f55863fbfbc0ddade7b8
Olexander Zakatov <sashazakatov17@gmail.com>
2023-08-16 11:34:19 +0300

    add GroupDeviceController

---
+++ app/Http/Controllers/GroupDeviceController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GroupDeviceController extends Controller
+{
+    public function create(){
+
+    }
+    public function get(){
+        
+    }
+}
