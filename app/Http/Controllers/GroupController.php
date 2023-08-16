f2da74a5e94ccf50b7dd775fb5bb38e1517dfe72
Olexander Zakatov <sashazakatov17@gmail.com>
2023-08-16 12:01:57 +0300

    update

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class GroupDeviceController extends Controller
+class GroupController extends Controller
 {
     public function create(){
 
