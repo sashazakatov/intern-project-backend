c478a632c3113ad6dd7f137f8fe1cbc657e29793
Olexander Zakatov <sashazakatov17@gmail.com>
2023-08-17 23:21:07 +0300

    Update UserCreateRequest.php

--- app/Http/Requests/UserCreateRequest.php
+++ app/Http/Requests/UserCreateRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\Rule;
 
 class UserCreateRequest extends FormRequest
 {
@@ -14,7 +15,11 @@ class UserCreateRequest extends FormRequest
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users,email',
             'surname' => 'required|string|max:255',
-            'role' => 'required|string|max:255',
+            'role' => [
+                'required',
+                'string',
+                Rule::in(['admin', 'regional_admin', 'owner', 'customer']),
+            ],
             'password' => 'required|string|max:255',
             'country' => 'required|string|max:255',
             'city' => 'required|string|max:255',
